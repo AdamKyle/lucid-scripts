f50ce90dcc4177ba776420a860cea403332d39ea
Adam Balan <a-kyle@hotmail.com>
2017-05-20 10:54:43 -0600

    Added the dist folder to the repo for people to get the scripts from.
    - Updated the readme.
    - We build the dev versions to build.
    - Updated docs
    - The script is 99% working.

--- src/lucid/event/map_events_icons.js
+++ src/lucid/event/map_events_icons.js
@@ -1,17 +1,31 @@
+/**
+ * Process all event icons on the map.
+ *
+ */
 module.exports = class MapEventsIcons {
 
+  /**
+   * Responsible for getting all event icons for a given map and all its events.
+   *
+   * @return {array} - array of objects.
+   */
   getAllEventIcons() {
     let eventIconObjects = [];
 
+    // Loop over the events.
     $gameMap._events.forEach((event) => {
       if (event !== undefined) {
         let eventPageCount = event.page().list.length;
 
+        // Loop over the event page count
         for (let i = 0; i < eventPageCount; i++) {
+          // If the event code is a comment.
           if (event.page().list[i].code === 108) {
+            // get the icon assuming there is a tag that matches:
             let icon = event.page().list[i].parameters[0].match(/<eventIcon: (.*)>/i);
 
             if (icon) {
+              // Create the icon object with event id and icon id.
               eventIconObjects.push({
                 event_id: event._eventId,
                 icon_id: Number(icon[1])
