71309007ab579ab5940929ca0ae31e40375bcb6b
Adam Balan <AdamKyleBalan@gmail.com>
2017-05-21 20:52:17 -0600

    From the merge.

--- src/lucid/event/map_events_icons.js
+++ src/lucid/event/map_events_icons.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 const extractAll = require('rmmv-mrp-core/option-parser').extractAll;
 
->>>>>>> b61f4e24e4bb97491a1ec3688c057284de885754
 /**
  * Process all event icons on the map.
  *
@@ -27,15 +24,9 @@ module.exports = class MapEventsIcons {
           // If the event code is a comment.
           if (event.page().list[i].code === 108) {
             // get the icon assuming there is a tag that matches:
-<<<<<<< HEAD
-            let icon = event.page().list[i].parameters[0].match(/<eventIcon: (.*)>/i);
-
-            if (icon) {
-=======
             const iconEventInfo = extractAll(event.page().list[i].parameters[0]);
 
             if (iconEventInfo.length > 0) {
->>>>>>> b61f4e24e4bb97491a1ec3688c057284de885754
               // Create the icon object with event id and icon id.
               eventIconObjects.push({
                 event_id: iconEventInfo[0].event || event._eventId,
@@ -61,30 +52,15 @@ module.exports = class MapEventsIcons {
    */
   getEventIcon(eventId) {
     const event = $gameMap.event(eventId);
-<<<<<<< HEAD
-=======
-
-    // if (event === undefined) {
-    //   return {event_id: 0, icon_id: 0};
-    // }
-
->>>>>>> b61f4e24e4bb97491a1ec3688c057284de885754
     const eventPageCount = event.page().list.length;
 
     for (let i = 0; i < eventPageCount; i++) {
       if (event.page().list[i].code === 108) {
-<<<<<<< HEAD
-        let icon = event.page().list[i].parameters[0].match(/<eventIcon: (.*)>/i);
-
-        if (icon) {
-          return { event_id: event._eventId, icon_id: Number(icon[1]) };
-=======
 
         let iconEventInfo = extractAll(event.page().list[i].parameters[0]);
 
         if (iconEventInfo.length > 0) {
           return { event_id: event._eventId, icon_id: Number(iconEventInfo[0].icon) };
->>>>>>> b61f4e24e4bb97491a1ec3688c057284de885754
         } else {
           return { event_id: event._eventId, icon_id: 0 }
         }
